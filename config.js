631c3e20823321e39cd7bfe7c55d2245f9ef1070
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-03-10 12:19:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ if (!SUDO) {
 
 global.owner = process.env.SUDO || ["919539412641"];
 module.exports = {
-  mongodb: process.env.MONGODB_URL || "mongodb+srv://ajsalsd:rioHWvIFV7nkxkWz@cluster0.d0hnfmp.mongodb.net/?retryWrites=true&w=majority",
+  mongodb: process.env.MONGODB_URL || "mongodb+srv://telaccww4f:x3oC7N2pSgTUzFYA@cluster0.pwjwmfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   BOT_INFO: process.env.BOT_INFO || "X-BOT-MD;Aswin Sparky;https://graph.org/file/9b477aaa6b90bbb46e4be.mp4;https://i.imgur.com/MsNQ8wf.jpg",
   STICKER_DATA: process.env.STICKER_DATA || "X-BOTMD;ASWIN SPARKY",
   SESSION_ID: process.env.SESSION_ID || "X-BOT-MD:NightyLikeness",
